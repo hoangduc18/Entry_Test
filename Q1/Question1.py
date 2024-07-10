284538ec609b3e470dc768b33a1fd9e2011d0ce2
hoangduc18 <67058601+hoangduc18@users.noreply.github.com>
2024-07-10 18:58:29 +0700

    Q1 Answer

---
+++ Q1/Question1.py
@@ -0,0 +1,31 @@
+import json
+def def_word_cnt(input):
+    words_array = input.split()
+    words_dict = {}
+    for word in words_array:
+        if words_dict.get(word) is not None:
+            word_cnt = words_dict.get(word)
+            word_cnt += 1
+            words_dict.update({word: word_cnt })
+        else:
+            words_dict[word] = 1
+    try:
+        with open("result.json", "w") as file:
+            json.dump(words_dict, file)
+        return "success"
+    except Exception as error:
+        print(error)
+
+print(def_word_cnt("hello i am Duc hello"))
+
+def recursive_file_generator(count):
+    if count < 5:
+        file_name = f"result_{count}"
+        f = open(f"{file_name}.json", "x")
+        f.close()
+        count += 1
+        recursive_file_generator(count)
+
+recursive_file_generator(1)
+
+    
